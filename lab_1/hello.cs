0457782875ddcb1530e0d10b8a1199034e19b15f
theanxiuser <bishalpoudel607@gmail.com>
2022-06-16 16:21:37 +0545

    lab 1

---
+++ lab_1/hello.cs
@@ -0,0 +1,7 @@
+using System;
+
+class helloEngineer{
+    static void Main(){
+	Console.WriteLine("Hello, Engineers!!");
+    }
+}
---
+++ lab_1/sum.cs
@@ -0,0 +1,7 @@
+using System;
+class Sum{
+    static void Main(){
+	int a=2, b=3;
+	Console.WriteLine("The sum of "+ a +" and "+ b +" is "+(a+b));
+    }
+}
