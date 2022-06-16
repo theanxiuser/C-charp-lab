620ea94e9de6c12ce51a47dfe67bf0d76293a3f9
theanxiuser <bishalpoudel607@gmail.com>
2022-06-16 16:22:09 +0545

    lab 3

---
+++ lab_3/user_input.cs
@@ -0,0 +1,9 @@
+using System;
+
+class GetInput{
+    public static void Main(){
+        Console.WriteLine("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Your name is: "+ name);
+    }
+}
